4d5ffb57bb795940b372b76769b0903e7dc61ace
VSNVV <victor.sanavia@edu.uah.es>
2022-12-21 15:19:10 +0100

    Realizado una idea basica de un menu de programa (tenemos que ver como lo podemos hacer, pero la idea esta ahi)

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ import os
 
 # Función para establecer conexxion con la base de datos
 # Función main (método principal)
-def main():
+def executer(command: str):
     '''
     En primer lugar tenemos que establecer la conexion al servidor, que en este caso es una base de datos
     '''
@@ -31,5 +31,24 @@ def main():
     connection.close()
 
 # Programa Principal
-
-main()
+opcion = 1
+while(not opcion.__eq__(0)):
+    print('\t\t-------------=[Menú Del Programa]=-------------\n')
+    print('\t1. Realizar una consulta\n\t2. Insertar datos\n\t3. Modificar datos\n\t4. Salir del programa')
+    opcion = int(input('\nElige opcion introduciendo su numero: '))
+    if opcion.__eq__(1):
+        command = str(input('Introduce la consulta SQL a realizar (con ; al final): '))
+        executer(command)
+    elif opcion.__eq__(2):
+        command = str(input('Introduce la consulta SQL a realizar (con ; al final): '))
+        if ('insert' in command):
+            executer(command)
+        else:
+            print('Esta consulta no inserta datos')
+    elif opcion.__eq__(3):
+        command = str(input('Introduce la consulta para insertar datos'))
+        executer(command)
+    elif opcion.__eq__(0):
+        break
+    else:
+        print('Opcion incorrecta o numero fuera de rango')
