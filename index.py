3b69c80b2a1545dc77e3c820ab11d10f4b9d0796
VSNVV <victor.sanavia@edu.uah.es>
2022-12-30 00:25:33 +0100

    Index bastante avanzado
    Falta por ver el tema del cursor.fetchall (no todas las tablas tienen el mismo numero de columnas)

--- index.py
+++ index.py
@@ -1,33 +1,103 @@
-# ------------ [LIBRERIAS NECESARIAS] ------------
+# ------------=[LIBRERIAS NECESARIAS]=------------
 
 import psycopg2
 from dotenv import load_dotenv
 import os
+import time
+load_dotenv()
 
-# ------------ [FUNCIONES DEFINIDAS] ------------
+# ------------=[VARIABLES GLOBALES]=------------
 
-# Función para establecer conexxion con la base de datos
-# Función executer, cada vez que se quiere enviar una consulta al servidor se deberá llamar a esta función
-def executer(command: str):
-    '''
-    En primer lugar tenemos que establecer la conexion al servidor, que en este caso es una base de datos
-    '''
-    load_dotenv()
-    server = os.getenv('server_info')
-    connection = psycopg2.connect(server)
-    
-    # Una vez la xonexión establecida, creamos el cursor para cargar el contenido de la consultas
+connection = psycopg2.connect(os.getenv('cliente_user'))
+cursor = connection.cursor()
+
+
+# ------------=[FUNCIONES DEFINIDAS]=------------
+
+# Función para elegir usuario
+def user_choice() -> str:
+
+    print('\t\t-----------------=[ELECCION DE USUARIO]=-----------------\n\n\t1. Admin\n\t2. Gestor\n\t3. Critico\n\t4. Cliente\n\n\t\t-----------------=[ELECCION DE USUARIO]=-----------------\n\n')
+    correct_choice = False
+
+    while (not correct_choice):
+        choice = int(input('Elige el usuario con el que deseas conectarte a la base de datos (numero): '))
+        if (choice.__eq__(1)):
+            # Se ha elegido el rol de usuario, por tanto se conectará con el rol de usuario de administrador
+            user = 'admin' + '_user'
+            correct_choice = True
+        elif (choice.__eq__(2)):
+            # Se ha elegido el usuario gestor
+            user = 'gestor' + '_user'
+            correct_choice = True
+        elif (choice.__eq__(3)):
+            # Se ha elegido el usuario critico
+            user = 'critico' + '_user'
+            correct_choice = True
+        elif (choice.__eq__(4)):
+            # Se ha elegido el usuario cliente
+            user = 'cliente' + '_user'
+            correct_choice = True
+        else:
+            print('Introduce un numero del 1 al 4')
+
+    user_info = os.getenv(user)
+
+    return user_info
+
+# Función para establecer conexion con la base de datos
+
+def connection_establishment(user_info: str):
+    global connection
+    global cursor
+    connection = psycopg2.connect(user_info)
     cursor = connection.cursor()
-    
-    cursor.execute('select * from peliculas.criticas;')
+
+# Funcion para terminar la conexion
+
+def connection_termination():
+
+    global connection
+    global cursor
+    cursor.close()
+    connection.close()
+    print('Conexion cerrada.')
+
+# Función executer, cada vez que se quiere enviar una consulta al servidor se deberá llamar a esta función
+
+def execute(sql_command: str):
+    global connection
+    global cursor
+    cursor.execute(sql_command)
 
     # El cursos cargará todos los valores de la tabla peliculas.criticas
     for critico, puntacion, texto, anno_peliculas, titulo_peliculas, nombre_pag_web, fecha in cursor.fetchall():
         print(critico, puntacion, texto, anno_peliculas, titulo_peliculas, nombre_pag_web, fecha)
-    
-    # Cerramos la conexión
 
-    cursor.close()
-    connection.close()
+# ------------=[PROGRAMA PRINCIPAL]=------------
 
-# Programa Principal
+# Primero debemos preguntar por qué usuario se va a realizar la consulta
+running = True
+
+while (running):
+    # Primero se deberá elegir el usuario, y acto seguido abrir la conexion con la base de datos
+    connection_establishment(user_choice())
+    # Escribimos la consulta a ejecutar:
+    sql_command = str(input('Introduce la consulta a realizar: '))
+    # Una vez escrita la consulta, la ejecutamos
+    execute(sql_command)
+    more_commands = str(input('\n\n¿Desea hacer mas consultas? (si / no): '))
+    succeded = False
+    while (not succeded):
+        if (more_commands.__eq__('no')):
+            connection_termination # Se termina la conexion
+            running = False # El programa acaba ejecucion
+            succeded = True
+        elif (more_commands.__eq__('si')):
+            succeded = True
+        else:
+            print('Escriba una opcion valida (si / no): ')
+
+print('Cerrando...')
+time.sleep(0.5)
+print('\t\t-----------------=[PROGRAMA FINALIZADO]=-----------------')
