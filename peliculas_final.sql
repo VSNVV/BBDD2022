1496fd35144769c3e09f55b8c76fb80bf783c989
oaoadrii <a.rodriguezh@edu.uah.es>
2022-12-29 23:32:08 +0100

    Añadido el esqueleto del trigger de auditoría

--- peliculas_final.sql
+++ peliculas_final.sql
@@ -14802,6 +14802,7 @@ GRANT INSERT ON peliculas.criticas TO critico; -- Un critico solo puede insertar
 GRANT SELECT ON ALL TABLES IN SCHEMA peliculas TO cliente; -- Un cliente solo puede consultar el contenido de las tablas
 
 -- En primer lugar tenemos que crear la tabla de auditoria, que guardará los eventos que tienen lugar en la base de datos
+
 CREATE TABLE peliculas.auditoria(
     evento text,
     tabla text,
@@ -14811,4 +14812,28 @@ CREATE TABLE peliculas.auditoria(
 
 -- Una vez creada la tabla de auditoria, podemos crear en trigger de esta misma tabla
 
+CREATE OR REPLACE FUNCTION fn_auditoria() RETURNS TRIGGER AS $fn_auditoria$
+  DECLARE
+  --  no declaro nada porque no me hace falta...de hecho DECLARE podría haberlo omitido en éste caso
+  BEGIN
+  -- Se determina que acción ha activado el trigger e inserta un nuevo valor en la tabla dependiendo de dicha acción
+  -- Junto con la acción se escribe la fecha y la hora en la que se ha producido la acción
+   IF TG_OP='INSERT' THEN
+     INSERT INTO auditoria VALUES ('alta',current_timestamp);  -- Cuando hay una inserción
+   ELSIF TG_OP='UPDATE'	THEN
+     INSERT INTO auditoria VALUES ('modificación',current_timestamp); -- Cuando hay una modificación
+   ELSEIF TG_OP='DELETE' THEN
+     INSERT INTO auditoria VALUES ('borrado',current_timestamp); -- Cuando hay un borrado
+   END IF;	 
+   RETURN NULL;
+  END;
+$fn_auditoria$ LANGUAGE plpgsql;
+
+
 -- Creamos la funcion que ejecutará el trigger
+
+-- Se crea el trigger que se dispara cuando hay una inserción, modificación o borrado en la tabla peliculas
+
+CREATE TRIGGER tg_auditoria after INSERT or UPDATE or DELETE
+  ON peliculas FOR EACH ROW
+  EXECUTE PROCEDURE fn_auditoria(); 
